7435371c390b4c7bc1ea479cfaad80e8de44d55a
Natália Frazão <110553454+Natalia-Frazao@users.noreply.github.com>
2023-01-26 13:06:36 -0300

    comit inicial

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class QueueExemplo
+{
+    public static void Main(String[] args)
+    {
+        Queue<int> q = new Queue<int>();
+
+        // Adiciona elementos {0, 1, 2, 3, 4} no queue
+        for (int i = 0; i < 5; i++)
+            q.Enqueue(i);
+
+        Console.Write("Elementos do queue-");
+        foreach (int s in q)
+            Console.Write(s + " ");
+
+
+        int remove = q.Dequeue();
+        Console.Write("\nremove elemento-" + remove + "\n");
+        foreach (int s in q)
+            Console.Write(s + " ");
+
+        // para ver a cabeça do queue
+        int cabeça = q.Peek();
+        Console.Write("\ncabeça do queue-" + cabeça);
+
+
+        // implementação.
+        int tamanho = q.Count;
+        Console.WriteLine("\ntamanho do queue-" + tamanho);
+        Queue<string> strQ = new Queue<string>();
+        strQ.Enqueue("o");
+        strQ.Enqueue("l");
+        strQ.Enqueue("l");
+        strQ.Enqueue("l");
+        strQ.Enqueue("á");
+
+        Console.WriteLine("Total elementos: {0}", strQ.Count); //prints 5
+
+        while (strQ.Count > 0)
+            Console.WriteLine(strQ.Dequeue()); //prints ollá
+
+        Console.WriteLine("Total elementos: {0}", strQ.Count); //prints 0
+
+       
+
+        Console.WriteLine("Tecle enter para fechar");
+                Console.ReadLine();
+
+
+
+            }
+}
---
+++ RoundRobin/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+public class GFG
+{
+
+    static void findWaitingTime(int[] processos,
+             int n, int[] bt, int[] wt, int quantum)
+    {
+
+
+        int[] rem_bt = new int[n];
+
+        for (int i = 0; i < n; i++)
+            rem_bt[i] = bt[i];
+
+        int t = 0;
+
+
+        while (true)
+        {
+            bool done = true;
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+                if (rem_bt[i] > 0)
+                {
+
+
+                    done = false;
+
+                    if (rem_bt[i] > quantum)
+                    {
+
+                        t += quantum;
+
+
+                        rem_bt[i] -= quantum;
+                    }
+
+
+                    else
+                    {
+
+
+                        t = t + rem_bt[i];
+
+
+                        wt[i] = t - bt[i];
+
+
+                        rem_bt[i] = 0;
+                    }
+                }
+            }
+
+
+            if (done == true)
+                break;
+        }
+    }
+
+
+    static void findTurnAroundTime(int[] processes,
+               int n, int[] bt, int[] wt, int[] tat)
+    {
+
+        for (int i = 0; i < n; i++)
+            tat[i] = bt[i] + wt[i];
+    }
+
+
+    static void findavgTime(int[] processos, int n,
+                             int[] bt, int quantum)
+    {
+        int[] wt = new int[n];
+        int[] tat = new int[n];
+        int total_wt = 0, total_tat = 0;
+
+
+        findWaitingTime(processos, n, bt, wt, quantum);
+
+
+        findTurnAroundTime(processos, n, bt, wt, tat);
+
+
+        Console.WriteLine("Processos " + " Tempo de Burst" +
+                    " Tempo de espera " + " Tempo de Turn Round");
+
+
+        for (int i = 0; i < n; i++)
+        {
+            total_wt = total_wt + wt[i];
+            total_tat = total_tat + tat[i];
+            Console.WriteLine(" " + (i + 1) + "\t\t" + bt[i]
+                         + "\t " + wt[i] + "\t\t " + tat[i]);
+        }
+
+        Console.WriteLine("Tempo médio de espera = " +
+                        (float)total_wt / (float)n);
+        Console.Write("Tempo médio de Turn Round = " +
+                        (float)total_tat / (float)n);
+    }
+
+
+    public static void Main()
+    {
+
+        int[] processos = { 1, 2, 3 };
+        int n = processos.Length;
+
+
+        int[] burst_time = { 10, 5, 8 };
+
+        // Tempo quantum
+        int quantum = 2;
+        findavgTime(processos, n, burst_time, quantum);
+    }
+}
